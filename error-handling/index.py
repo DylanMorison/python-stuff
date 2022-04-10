8d2576b1974cc68945ef560e4d0573e199094cfa
DylanMorison <71316513+DylanMorison@users.noreply.github.com>
2022-04-09 17:37:12 -0700

    rename & error handling

---
+++ error-handling/index.py
@@ -0,0 +1,26 @@
+def add_three_nums(a, b, c):
+    try:
+        result = a + b + int(c)
+        print(result)
+    except:
+        print("caught error in add_three_nums func!")
+    else:
+        print("add went well!")
+
+
+# a = 1
+# b = 1
+# c = input("please enter a number: ")
+# add_three_nums(a, b, c)
+
+try:
+    f = open("testfile.txt", "w")
+    f.write("write a test line\nAnother test line")
+except TypeError:
+    print("There was a type error!")
+except OSError:
+    print("Hey you have an IO error!")
+except:
+    print("I catch ALL errors!!!")
+finally:
+    print("I always run!")
