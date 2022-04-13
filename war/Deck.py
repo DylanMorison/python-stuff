184d442a3b19fe0818db7270b2ef61cd61ae13bc
DylanMorison <71316513+DylanMorison@users.noreply.github.com>
2022-04-12 20:42:14 -0700

    wip

--- war/Deck.py
+++ war/Deck.py
@@ -4,9 +4,7 @@ from Card import suits, ranks, Card
 
 class Deck:
     def __init__(self):
-
         self.all_cards = []
-
         for suit in suits:
             for rank in ranks:
                 created_card = Card(suit, rank)
@@ -15,12 +13,17 @@ class Deck:
     def shuffle(self):
         shuffle(self.all_cards)
 
+    def deal_one(self):
+        return self.all_cards.pop()
+
     def print_cards(self):
         for card in self.all_cards:
             print(card)
 
 
 new_deck = Deck()
-new_deck.print_cards()
 new_deck.shuffle()
-new_deck.print_cards()
+print(len(new_deck.all_cards))
+mycard = new_deck.deal_one()
+print(mycard)
+print(len(new_deck.all_cards))
