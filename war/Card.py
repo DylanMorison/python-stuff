a2bc26f6173004d1d3f3adaf184b44c3a3257952
DylanMorison <71316513+DylanMorison@users.noreply.github.com>
2022-04-12 20:39:11 -0700

    Update Card.py

--- war/Card.py
+++ war/Card.py
@@ -12,9 +12,8 @@ class Card:
         self.value = values[rank]
 
     def __str__(self):
-        return self.rank + " of" + self.suit
+        return self.rank + " of " + self.suit
 
 
 two_hearts = Card("Hearts", "Two")
 three_of_clubs = Card("Clubs","Three")
-print(three_of_clubs)
