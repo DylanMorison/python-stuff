c3917e6ecd51d9a0640531b1cc361a7a35988cc4
DylanMorison <71316513+DylanMorison@users.noreply.github.com>
2022-04-12 20:33:41 -0700

    Create Deck.py

---
+++ war/Deck.py
@@ -0,0 +1,12 @@
+from Card import suits, ranks, Card
+
+
+class Deck:
+    def __init__(self):
+
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                created_card = Card(suit, rank)
+                self.all_cards.append(created_card)
