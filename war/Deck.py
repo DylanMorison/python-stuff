f2f7cfa98d1ed2413bfbafb38775bb31022731d7
DylanMorison <71316513+DylanMorison@users.noreply.github.com>
2022-04-12 20:39:15 -0700

    Update Deck.py

--- war/Deck.py
+++ war/Deck.py
@@ -1,3 +1,4 @@
+from random import shuffle
 from Card import suits, ranks, Card
 
 
@@ -10,3 +11,16 @@ class Deck:
             for rank in ranks:
                 created_card = Card(suit, rank)
                 self.all_cards.append(created_card)
+
+    def shuffle(self):
+        shuffle(self.all_cards)
+
+    def print_cards(self):
+        for card in self.all_cards:
+            print(card)
+
+
+new_deck = Deck()
+new_deck.print_cards()
+new_deck.shuffle()
+new_deck.print_cards()
